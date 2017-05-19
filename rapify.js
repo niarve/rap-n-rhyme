cf2425c046a27372339377efb3847803f9a4a25c
Nathan Iarve <NIarve@manta.com>
2017-05-19 10:04:11 -0400

    found better api for rapping with

--- rapify.js
+++ rapify.js
@@ -4,8 +4,9 @@ module.exports = function(line) {
   const words = line.split(' ');
   const word = words[words.length - 1];
 
-  request("https://api.datamuse.com/words?sl=" + word, (err, response, body) => {
-    rhymes = JSON.parse(body);
-    rhymes.forEach((rhyme) => console.log(rhyme.word));
+  request("http://rhymebrain.com/talk?function=getRhymes&word==" + word, (err, response, body) => {
+    response = JSON.parse(body);
+    rhymes = response.filter(rhyme => rhyme.score >= 300)
+      .map(r => console.log(r.word));
   });
 }
